11a5239be63cbcb06d35a81db2b2488aaf70ac46
AdisonP <52446207+AdisonPDO@users.noreply.github.com>
2024-05-17 10:18:14 +0200

    up

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RacketComparison from './view/racket_comparator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RacketComparison />
   </React.StrictMode>
 );
 
